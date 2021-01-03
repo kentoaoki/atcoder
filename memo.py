79241d6654bc935a3411df9a8b74367fab70496b
Kento Aoki <aokikento@Kentos-MacBook-Pro.local>
2021-01-04 07:44:15 +0900

    add dijkstra

--- memo.py
+++ memo.py
@@ -1,42 +1,37 @@
-import math
-from math import gcd,pi,sqrt
-INF = float("inf")
-MOD = 10**9 + 7
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-import itertools
-import bisect
-import re
-from collections import Counter,deque,defaultdict
-def iinput(): return int(input())
-def imap(): return map(int, input().split())
-def ilist(): return list(imap())
-def irow(N): return [iinput() for i in range(N)]
-def sinput(): return input().rstrip()
-def smap(): return sinput().split()
-def slist(): return list(smap())
-def srow(N): return [sinput() for i in range(N)]
+def dijkstra(G,s):
+    """ヒープ使用のダイクストラ法により,辺の重みが非負グラフの最短経路を求める O(|E|log|V|)
+    
+    Args:
+        G: 重み付きグラフ((頂点,重み)の隣接リスト形式)
+        s: 探索開始頂点
+    
+    Returns:
+        dist: s(探索開始頂点)からの最短距離.INFの場合は到達しない
+        
+    """
+    INF = float('inf')
+    dist = [INF]*N
+    dist[s] = 0
 
-def main():
-    import numpy as np
-    n,c = imap()
-    l = np.array([ilist() for _ in range(n)])
-    right = np.cumsum(l[:,1]) - l[:,0]
-    right_max = np.maximum.accumulate(right)
-    left_dist = c - l[:,0][::-1]
-    left = np.cumsum(l[:,1][::-1]) - left_dist
-    left_max = np.maximum.accumulate(left)
-    # print(left_dist)
-    right_to_left = right[:-1] - l[:,0][:-1] + left_max[:-1][::-1]
-    left_to_right = left[:-1] - left_dist[:-1] + right_max[:-1][::-1]
-    ans = max(np.max(right), np.max(left))
-    if len(right_to_left):
-        ans = max(ans, np.max(right_to_left))
-        ans = max(ans, np.max(left_to_right))
-    print(ans if ans > 0 else 0)
-    # print(right,left,right_to_left,left_to_right)
+    # ヒープの初期化
+    import heapq
+    que = [[dist[s], s]]
+    heapq.heapify(que)
 
+    while que:
+        d, v = heapq.heappop(que) # d最小値ペアの取り出し
+        if d > dist[v]: # ゴミ処理
+            continue
+        for e in G[v]:
+            if dist[e[0]] > dist[v] + e[1]:
+                dist[e[0]] = dist[v] + e[1]
+                heapq.heappush(que, [dist[e[0]], e[0]])
+    return dist
 
-if __name__=="__main__":
-    main()
+N,M,s = map(int, input().split())
+G = [[] for i in range(N)]
+for i in range(M):
+    a,b,w = map(int, input().split())
+    G[a].append([b,w])
+ans = dijkstra(G,s)
+print(*map(lambda x: x if x != float('inf') else 'INF', ans), sep='\n')
--- start.py
+++ start.py
@@ -298,7 +298,7 @@ def meguru_bisect(ng, ok):
             ng = mid
     return ok
 
-### ダイクストラ法
+### ダイクストラ法(線形探索)
 
 def dijkstra(G,s):
     """ダイクストラ法により、辺の重みが非負グラフの最短経路を求める O(|V|^2)
@@ -337,6 +337,38 @@ for i in range(M):
     G[a].append([b,w])
 ans = dijkstra(G,s)
 ans
+
+### ダイクストラ法(heapq)
+
+def dijkstra(G,s):
+    """ヒープ使用のダイクストラ法により,辺の重みが非負グラフの最短経路を求める O(|E|log|V|)
+    
+    Args:
+        G: 重み付きグラフ((頂点,重み)の隣接リスト形式)
+        s: 探索開始頂点
+    
+    Returns:
+        dist: s(探索開始頂点)からの最短距離.INFの場合は到達しない
+        
+    """
+    INF = float('inf')
+    dist = [INF]*N
+    dist[s] = 0
+
+    # ヒープの初期化
+    import heapq
+    que = [[dist[s], s]]
+    heapq.heapify(que)
+
+    while que:
+        d, v = heapq.heappop(que) # d最小値ペアの取り出し
+        if d > dist[v]: # ゴミ処理
+            continue
+        for e in G[v]:
+            if dist[e[0]] > dist[v] + e[1]:
+                dist[e[0]] = dist[v] + e[1]
+                heapq.heappush(que, [dist[e[0]], e[0]])
+    return dist
 
 ### ワーシャルフロイド法
 
